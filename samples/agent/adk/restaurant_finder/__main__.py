c339aacf62a553948a5358345ab3170a4091a456
ramonankersmit <ramon_ankersmit@hotmail.com>
2026-01-08 20:56:38 +0100

    fix(cors): allow 127.0.0.1 origin for local A2UI shell

--- samples/agent/adk/restaurant_finder/__main__.py
+++ samples/agent/adk/restaurant_finder/__main__.py
@@ -89,7 +89,7 @@ def main(host, port):
 
         app.add_middleware(
             CORSMiddleware,
-            allow_origin_regex=r"http://localhost:\d+",
+            allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
